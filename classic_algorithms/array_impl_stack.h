052eb3f28756f0d6a061459764a9f5dbd7d9cab6
vard <an.skornyakov@mail.ru>
2013-05-24 18:57:15 +0400

    + Stack array implementation

--- classic_algorithms/array_impl_stack.h
+++ classic_algorithms/array_impl_stack.h
@@ -1,4 +1,58 @@
 #ifndef __ARRAY_IMPL_STACK__
 #define __ARRAY_IMPL_STACK__
 
+namespace array_impl_stack
+{
+#define DEFAULT_STACK_DEPTH 10
+
+    template <typename T> class Stack{
+    private:
+        T* stackArray;
+        unsigned int stackPtr;    
+        unsigned int arraySize;
+    public:
+        Stack();
+        ~Stack();
+        bool isEmpty();
+        void push(T& item);
+        T pop();
+    };
+
+    template <typename T> Stack<T>::Stack() : stackPtr(0), arraySize(DEFAULT_STACK_DEPTH){
+        stackArray = new T[arraySize];
+    }
+
+    template <typename T> Stack<T>::~Stack(){
+        delete[] stackArray;
+    }
+
+    template <typename T> bool Stack<T>::isEmpty(){
+        return (stackPtr==0);
+    }
+
+    template <typename T> void Stack<T>::push(T &item){
+        *(stackArray+stackPtr) = item;
+        stackPtr++;
+        if(stackPtr==arraySize){
+            T* newArray = new T[2*arraySize];
+            memcpy(newArray, stackArray, arraySize*sizeof(T));
+            delete[] stackArray;
+            stackArray = newArray;
+            arraySize*=2;
+        }
+    }
+
+    template <typename T> T Stack<T>::pop(){
+        T val = *(stackArray+stackPtr-1);
+        stackPtr--;
+        if(stackPtr<=arraySize/4){
+            T* newArray = new T[arraySize/2];
+            memcpy(newArray, stackArray, stackPtr*sizeof(T));
+            delete[] stackArray;
+            stackArray = newArray;
+            arraySize/=2;
+        }
+        return val;
+    }
+}
 #endif 
--- classic_algorithms/classic_algorithms.cpp
+++ classic_algorithms/classic_algorithms.cpp
@@ -2,17 +2,18 @@
 //
 
 #include "stdafx.h"
-#include "my_stack.h"
-#include "array_impl_stack"
+#include "linklist_stack.h"
+#include "array_impl_stack.h"
 #include <iostream>
 
 using namespace std;
-
+using namespace linked_list_stack;
+using namespace array_impl_stack;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
     /* linked list implementation stack */
-    Stack<int> integerStack;
+    linked_list_stack::Stack<int> integerStack;
     for(int i = 0; i < 100; i++){
         integerStack.push(i);
     }
@@ -25,6 +26,24 @@ int _tmain(int argc, _TCHAR* argv[])
     while(!integerStack.isEmpty()){
         cout << integerStack.pop() << " ";
     }
+
+    cout << endl;
+
+        /* array implementation stack */
+    array_impl_stack::Stack<int> integerArrayBasedStack;
+    for(int i = 0; i < 100; i++){
+        integerArrayBasedStack.push(i);
+    }
+    for(int i = 0; i < 50; i++){
+        cout << integerArrayBasedStack.pop() << " ";
+    }
+    /*
+    for(int i = 0; i < 100; i++){
+        integerArrayBasedStack.push(i);
+    }
+    while(!integerArrayBasedStack.isEmpty()){
+        cout << integerArrayBasedStack.pop() << " ";
+    }*/
 
 	return 0;
 }
